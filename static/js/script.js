4b0d42d59c4181409a8b144b905dbc3651e494c3
DeniQu <108462245+DeniQu@users.noreply.github.com>
2023-12-28 20:16:25 +0000

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -21,7 +21,7 @@
 // Функция для загрузки задач с бэкенда и отображения на странице
 async function loadTasks() {
   try {
-    const response = await fetch('https://tg-crm-backend.onrender.com/task/?format=json');
+    const response = await fetch('https://tg-crm-backend.onrender.com/task/');
     if (!response.ok) {
       throw new Error('Failed to fetch tasks');
     }
